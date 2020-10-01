3c451901c085532f36de27ae783496679f87a4ac
Allen Porter <allen@thebends.org>
2020-10-01 06:13:38 +0000

    Remove unnecessary comments and error handling
    Remove error handling that simply wraps api exceptions already handled
    by DataUpdateCoordinator.  Seems better not to swallow unnecessarily.

--- homeassistant/components/google_nest/const.py
+++ homeassistant/components/google_nest/const.py
@@ -6,7 +6,8 @@ OAUTH2_AUTHORIZE = (
     "https://nestservices.google.com/partnerconnections/{project_id}/auth"
 )
 OAUTH2_TOKEN = "https://www.googleapis.com/oauth2/v4/token"
-
 SDM_SCOPES = ["https://www.googleapis.com/auth/sdm.service"]
 
 API_URL = "https://smartdevicemanagement.googleapis.com/v1/enterprises/{project_id}"
+
+POLLING_INTERVAL_SEC = 30
